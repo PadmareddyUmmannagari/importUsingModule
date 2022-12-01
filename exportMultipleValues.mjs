0f98236330d2ff049a9a3fddf5e328f99ec49f40
root <u.padmareddy2492@gmail.com>
2022-12-01 01:57:58 +0000

    Es6 module

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,8 @@
+let myArray = ["camel", 265, true, "5.6"];
+let bulb = { watts: 10, type: "LED" };
+function multiplyByFour(num) {
+  if (num % 4 === 0) {
+    return num;
+  }
+}
+export { myArray, bulb, multiplyByFour };
